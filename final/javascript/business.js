afab830ed35a51c3ffea3387366fcaea1693da59
Preston Gunter <prestongunter@yahoo.com>
2021-04-07 13:21:10 -0600

    update

---
+++ final/javascript/business.js
@@ -0,0 +1,37 @@
+const requestURL = 'https://raw.githubusercontent.com/McGuffinr/mcguffin.github.io/master/final-design/jason/business.json';
+fetch(requestURL)
+.then(function (response) {
+   return response.json();
+})
+	
+  .then(function (jsonObject) {
+    const business = jsonObject["business"];
+
+    for (let i = 0; i < business.length; i++ ) {
+        let card = document.createElement('section');
+        let h2 = document.createElement('h2');
+        let image = document.createElement('img');
+        let  contact_info = document.createElement('div');
+        let  link = document.createElement('a');
+
+      h2.textContent = business[i].name;
+
+      image.setAttribute('src', business[i].logo);
+
+      image.setAttribute('alt', business[i].name);
+
+      contact_info.textContent = business[i].contactinformation;
+
+      link.textContent = business[i].weblink; 
+
+      card.appendChild(h2);
+      card.appendChild(image);
+      card.appendChild(contact_info);
+      card.appendChild(link);
+      
+      document.querySelector('div.card').appendChild(card);
+    }
+
+  });
+
+  const business = jsonObject['business'];
